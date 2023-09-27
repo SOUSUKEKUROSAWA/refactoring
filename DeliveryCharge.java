07188fcd0562e6d67b2dbd40f5f7eb7478e81de0
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2023-09-27 22:45:38 +0900

    ひな型からリファクタリング対象のロジックを呼び出し

--- DeliveryCharge.java
+++ DeliveryCharge.java
@@ -2,12 +2,6 @@ class DeliveryCharge {
     final int amount;
 
     DeliveryCharge(final ShoppingCart shoppingCart) {
-        int totalPrice = shoppingCart.products.get(0).price + shoppingCart.products.get(1).price;
-        if (totalPrice < 2000) {
-            amount = 500
-        }
-        else {
-            amount = 0;
-        }
+        amount = DeliveryMangager.deliveryCharge(shoppingCart.products);
     }
 }
