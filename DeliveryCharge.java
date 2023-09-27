4ce3e4b4f631671e66c3050cf49adda0f322c619
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2023-09-27 23:08:18 +0900

    マジックナンバーを定数へ置き換え

--- DeliveryCharge.java
+++ DeliveryCharge.java
@@ -1,12 +1,15 @@
 class DeliveryCharge {
+    private static final int CHARGE_FREE_THERESHOLD = 2000;
+    private static final int PAY_CHARGE = 500;
+    private static final int CHARGE_FREE = 0;
     final int amount;
 
     DeliveryCharge(final ShoppingCart shoppingCart) {
-        if (shoppingCart.totalPrice() < 2000) {
-            amount = 500;
+        if (shoppingCart.totalPrice() < CHARGE_FREE_THERESHOLD) {
+            amount = PAY_CHARGE;
         }
         else {
-            amount = 0;
+            amount = CHARGE_FREE;
         }
     }
 }
