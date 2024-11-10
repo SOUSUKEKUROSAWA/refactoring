e37043931228aa1c1860c2cfa21c56b04f759477
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2024-11-10 14:54:31 +0900

    問い合わせによる一時変数の置き換え

--- 1/print-invoices.js
+++ 1/print-invoices.js
@@ -5,7 +5,7 @@ function statement (invoice, plays) {
     const format = new Intl.NumberFormat("en-US",{ style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
 
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
+        const play = playFor(perf);
         let thisAmount = amountFor(perf, play);
     
         // add volume credits
@@ -20,6 +20,10 @@ function statement (invoice, plays) {
     result += `Amount owed is ${format(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
 }
 
 function amountFor(aPerformance, play) {
