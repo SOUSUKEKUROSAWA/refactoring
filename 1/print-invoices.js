a5a49208330ea3186839fcf126bb52bd79608eac
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2024-11-10 17:48:31 +0900

    変数のインライン化

--- 1/print-invoices.js
+++ 1/print-invoices.js
@@ -4,9 +4,8 @@ function statement (invoice, plays) {
     for (let perf of invoice.performances) {    
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
-    let totalAmount = appleSauce();
 
-    result += `Amount owed is ${usd(totalAmount)}\n`;
+    result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 
@@ -62,11 +61,11 @@ function statement (invoice, plays) {
         return volumeCredits;
     }
 
-    function appleSauce() {
-        let totalAmount = 0;
+    function totalAmount() {
+        let result = 0;
         for (let perf of invoice.performances) {    
-            totalAmount += amountFor(perf);
+            result += amountFor(perf);
         }
-        return totalAmount;
+        return result;
     }
 }
